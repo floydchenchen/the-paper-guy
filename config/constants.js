f7d03eb2f21de02963f1905c68e5f3d6ffbad1cd
Floyd Chen <chen2918@umn.edu>
2019-04-07 11:51:38 -0400

    change config dir path

---
+++ config/constants.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyALUnM7wUNq8CANLp57bHWqqdLJDc1S1AE",
+  authDomain: "the-paper-guy.firebaseapp.com",
+  databaseURL: "https://the-paper-guy.firebaseio.com",
+  projectId: "the-paper-guy",
+  storageBucket: "the-paper-guy.appspot.com",
+  messagingSenderId: "313471211831"
+};
+
+export default config;
